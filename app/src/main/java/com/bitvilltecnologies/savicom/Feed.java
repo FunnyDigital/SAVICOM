adbb429745f9a4342d577a59fde2f9f366c0768b
Isolokwu Patrick C <isolokwupatrickc@gmail.com>
2021-01-30 23:08:43 -1100

    feed

--- app/src/main/java/com/bitvilltecnologies/savicom/Feed.java
+++ app/src/main/java/com/bitvilltecnologies/savicom/Feed.java
@@ -2,14 +2,25 @@ package com.bitvilltecnologies.savicom;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Feed extends AppCompatActivity {
+    ImageView imageViewno;
+    RecyclerView recyclerView;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference reference;
+    LinearLayoutManager mLayoutManager;
+
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -44,6 +55,25 @@ public class Feed extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setSelectedItemId(R.id.newsfeed);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        mLayoutManager = new LinearLayoutManager(Feed.this);
+        mLayoutManager.setReverseLayout(true);
+        mLayoutManager.setStackFromEnd(true);
+
+
+        recyclerView = findViewById(R.id.recycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager(mLayoutManager);
+
+
+
+        firebaseDatabase =FirebaseDatabase.getInstance();
+        reference= firebaseDatabase.getReference("Users/POSTS");
+
+        //imageViewno=findViewById(R.id.imageViewno);
+        if (!TextUtils.isEmpty(Config.imageLink))
+            Picasso.get().load(Config.imageLink).into(imageViewno);
     }
 
 
